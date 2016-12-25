f819410d6e2a8794b8919662ce8cb14259f3cdc6
Supanut Apikulvanich <supanut.overload@gmail.com>
2016-12-25 08:49:44 +0700

    Random background color

--- js/roulette.js
+++ js/roulette.js
@@ -82,6 +82,31 @@ function drawRouletteShadow(){
 
 }
 
+function HSVtoRGB(h, s, v) {
+    var r, g, b, i, f, p, q, t;
+    if (arguments.length === 1) {
+        s = h.s, v = h.v, h = h.h;
+    }
+    i = Math.floor(h * 6);
+    f = h * 6 - i;
+    p = v * (1 - s);
+    q = v * (1 - f * s);
+    t = v * (1 - (1 - f) * s);
+    switch (i % 6) {
+        case 0: r = v, g = t, b = p; break;
+        case 1: r = q, g = v, b = p; break;
+        case 2: r = p, g = v, b = t; break;
+        case 3: r = p, g = q, b = v; break;
+        case 4: r = t, g = p, b = v; break;
+        case 5: r = v, g = p, b = q; break;
+    }
+    return {
+        r: Math.round(r * 255),
+        g: Math.round(g * 255),
+        b: Math.round(b * 255)
+    };
+}
+
 function drawArcs(){
   //TODO: cleanup this two duplicated for
   var startAngle, endAngle = 0;
@@ -98,6 +123,8 @@ function drawArcs(){
 
     var d = "M" + center.x + "," + center.y + "L" + x1 + "," + y1 + " A" + diameter + "," + diameter + " 0 0,1 " + x2 + "," + y2 + " z"; //1 means clockwise
     arc = paper.path(d);
+    // var colorRGB = HSVtoRGB(i / multiplyList(pieText).length, 0.7, 0.5);
+    // document.body.style.backgroundColor = 'rgb(' + [colorRGB.r, colorRGB.g, colorRGB.b].join(',') + ')';
     arc.attr("fill", getColor(i, multiplyList(pieText).length));
     arcs.push(arc);
   }
@@ -169,7 +196,10 @@ function randomSpin(){
 function updateResult(id) {
   var i = 0;
   var waitingResult = setInterval(function() {
-    document.getElementById('result').innerHTML = pieText[(i++) % len_choices];
+    document.getElementById('result').innerHTML = pieText[i % len_choices];
+    var colorRGB = HSVtoRGB(i / multiplyList(pieText).length, 0.7, 0.5);
+    document.body.style.backgroundColor = 'rgb(' + [colorRGB.r, colorRGB.g, colorRGB.b].join(',') + ')';
+    i = (i + 1) % len_choices;
   }, 100);
 
   // setTimeout(function() {
@@ -180,6 +210,8 @@ function updateResult(id) {
     console.log(id);
     clearInterval(waitingResult);
     document.getElementById('result').innerHTML = pieText[id % len_choices];
+    var colorRGB = HSVtoRGB(id / multiplyList(pieText).length, 0.7, 0.5);
+    document.body.style.backgroundColor = 'rgb(' + [colorRGB.r, colorRGB.g, colorRGB.b].join(',') + ')';
   }, time * (8.0-2.5) / 8.0);
 }
 
